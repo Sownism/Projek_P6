b95828cc5674ea631e5daaa3117afd7e313c5fc9
PERSONAL <PERSONAL@DESKTOP-I5A08OP>
2025-04-30 18:12:23 +0700

    Add project files.

---
+++ coba_tugas/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace coba_tugas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.buttonTambah = new System.Windows.Forms.Button();
+            this.buttonUbah = new System.Windows.Forms.Button();
+            this.buttonHapus = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.buttonCari = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(67, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(655, 306);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Font = new System.Drawing.Font("PoetsenOne", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.Location = new System.Drawing.Point(86, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Judul Buku :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(219, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Font = new System.Drawing.Font("PoetsenOne", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(86, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nama Penulis :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(219, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label3.Font = new System.Drawing.Font("PoetsenOne", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(86, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Genre :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(219, 134);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(170, 22);
+            this.textBox3.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label4.Font = new System.Drawing.Font("PoetsenOne", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(86, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 22);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Deskripsi :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(219, 174);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(279, 75);
+            this.textBox4.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(67, 360);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(655, 232);
+            this.dataGridView2.TabIndex = 3;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            // 
+            // buttonTambah
+            // 
+            this.buttonTambah.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTambah.Location = new System.Drawing.Point(68, 325);
+            this.buttonTambah.Name = "buttonTambah";
+            this.buttonTambah.Size = new System.Drawing.Size(90, 26);
+            this.buttonTambah.TabIndex = 5;
+            this.buttonTambah.Text = "Tambah";
+            this.buttonTambah.UseVisualStyleBackColor = true;
+            // 
+            // buttonUbah
+            // 
+            this.buttonUbah.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUbah.Location = new System.Drawing.Point(164, 325);
+            this.buttonUbah.Name = "buttonUbah";
+            this.buttonUbah.Size = new System.Drawing.Size(75, 26);
+            this.buttonUbah.TabIndex = 6;
+            this.buttonUbah.Text = "Update";
+            this.buttonUbah.UseVisualStyleBackColor = true;
+            // 
+            // buttonHapus
+            // 
+            this.buttonHapus.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonHapus.Location = new System.Drawing.Point(245, 325);
+            this.buttonHapus.Name = "buttonHapus";
+            this.buttonHapus.Size = new System.Drawing.Size(80, 26);
+            this.buttonHapus.TabIndex = 7;
+            this.buttonHapus.Text = "Hapus";
+            this.buttonHapus.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(420, 335);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cari Kode Buku";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(525, 332);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(137, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // buttonCari
+            // 
+            this.buttonCari.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCari.Location = new System.Drawing.Point(668, 331);
+            this.buttonCari.Name = "buttonCari";
+            this.buttonCari.Size = new System.Drawing.Size(53, 23);
+            this.buttonCari.TabIndex = 10;
+            this.buttonCari.Text = "Cari";
+            this.buttonCari.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label6.Font = new System.Drawing.Font("PoetsenOne", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(89, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Kode Buku";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(219, 18);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(170, 22);
+            this.textBox6.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(788, 637);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.buttonCari);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonHapus);
+            this.Controls.Add(this.buttonUbah);
+            this.Controls.Add(this.buttonTambah);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox textBox3;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.TextBox textBox4;
+        public System.Windows.Forms.Button buttonTambah;
+        public System.Windows.Forms.Button buttonUbah;
+        public System.Windows.Forms.Button buttonHapus;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button buttonCari;
+        public System.Windows.Forms.TextBox textBox6;
+        public System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ coba_tugas/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace coba_tugas
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection conn;
+        private DataTable dataTable;
+        private string connectionString = "server=localhost;user id=root;password=;database=novel;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection(connectionString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDataNovel();
+
+            buttonTambah.Click += buttonTambah_Click;
+            buttonUbah.Click += buttonUbah_Click;
+            buttonHapus.Click += buttonHapus_Click;
+            buttonCari.Click += buttonCari_Click;
+        }
+
+        private void LoadDataNovel()
+        {
+            string query = "SELECT * FROM buku";
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dataGridView2.DataSource = null;
+                    dataGridView2.DataSource = dt;
+
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message);
+                }
+            }
+        }
+  
+
+        private void buttonTambah_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox6.Text) || string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Kode buku dan judul harus diisi!");
+                return;
+            }
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "INSERT INTO buku (kode_buku, judul_buku, nama_penulis, genre, deskripsi) " +
+                                   "VALUES (@kode, @judul, @penulis, @genre, @deskripsi)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@kode", textBox6.Text);
+                    cmd.Parameters.AddWithValue("@judul", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@penulis", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@genre", textBox3.Text);
+                    cmd.Parameters.AddWithValue("@deskripsi", textBox4.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data berhasil ditambahkan!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal menambahkan data: " + ex.Message);
+                }
+            }
+
+            LoadDataNovel();  // Panggil setelah proses selesai
+            ClearForm();
+        }
+
+
+        private void buttonUbah_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox6.Text))
+            {
+                MessageBox.Show("Pilih data yang akan diubah terlebih dahulu!");
+                return;
+            }
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "UPDATE buku SET judul_buku=@judul, nama_penulis=@penulis, genre=@genre, deskripsi=@deskripsi " +
+                                   "WHERE kode_buku=@kode";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@kode", textBox6.Text);
+                    cmd.Parameters.AddWithValue("@judul", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@penulis", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@genre", textBox3.Text);
+                    cmd.Parameters.AddWithValue("@deskripsi", textBox4.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data berhasil diubah!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal mengubah data: " + ex.Message);
+                }
+            }
+
+            LoadDataNovel();
+            ClearForm();
+        }
+
+
+        private void buttonHapus_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox6.Text))
+            {
+                MessageBox.Show("Pilih data yang akan dihapus terlebih dahulu!");
+                return;
+            }
+
+            if (MessageBox.Show("Apakah Anda yakin ingin menghapus data ini?", "Konfirmasi",
+                MessageBoxButtons.YesNo) == DialogResult.Yes)
+
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "DELETE FROM buku WHERE kode_buku = @kode";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@kode", textBox6.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data berhasil dihapus.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal menghapus data: " + ex.Message);
+                }
+            }
+
+            LoadDataNovel();
+            ClearForm();
+        }
+
+
+        private void buttonCari_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM buku WHERE kode_buku = @kode";
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
+                    da.SelectCommand.Parameters.AddWithValue("@kode", textBox5.Text);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dataGridView2.DataSource = null;
+                    dataGridView2.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal mencari data: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView2.Rows[e.RowIndex];
+                textBox6.Text = row.Cells["kode_buku"].Value.ToString();
+                textBox1.Text = row.Cells["judul_buku"].Value.ToString();
+                textBox2.Text = row.Cells["nama_penulis"].Value.ToString();
+                textBox3.Text = row.Cells["genre"].Value.ToString();
+                textBox4.Text = row.Cells["deskripsi"].Value.ToString();
+            }
+        }
+
+        private void ClearForm()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label4_Click(object sender, EventArgs e) { }
+    }
+}
